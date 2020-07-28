264fe4a5138a44efc56b54439b1cb9693d8e6432
umesh013 <49593186+umesh013@users.noreply.github.com>
2020-07-28 07:34:41 +0545

    Updated server and client programs

--- Socket_Prog_Usth/client.py
+++ Socket_Prog_Usth/client.py
@@ -1,17 +1,51 @@
 import socket
+import sys
+import select
+import errno
+HEADER_LENGTH = 10
+IP = "127.0.0.1"
+port = 1024
+#host = socket.gethostname()  # as both code is running on same pc
+my_username = input("Username: ")
 
-host = socket.gethostname()  # as both code is running on same pc
-port = 5000  # socket server port number
 
-client_socket = socket.socket()  # instantiate
-client_socket.connect((host, port))  # connect to the server
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # instantiate
+client_socket.connect((IP, port))  # connect to the server
 
-message = input(" -> ")  # take input
+client_socket.setblocking(False)
+username = my_username.encode('utf-8')
+username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
+client_socket.send(username_header + username)
+while True:
+    message = input(f'{my_username} > ')
+    if message:
+        # Encode message to bytes, prepare header and convert to bytes, like for username above, then send
+        message = message.encode('utf-8')
+        message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+        client_socket.send(message_header + message)
+    try:
+        while True:
+            username_header = client_socket.recv(HEADER_LENGTH)
 
-while message.lower().strip() != 'bye':
-    client_socket.send(message.encode())  # send message
-    data = client_socket.recv(1024).decode()  # receive response
+            # If we received no data, server gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
+            if not len(username_header):
+                print('Connection closed by the server')
+                sys.exit()
+            username_length = int(username_header.decode('utf-8').strip())
+            username = client_socket.recv(username_length).decode('utf-8')
+            message_header = client_socket.recv(HEADER_LENGTH)
+            message_length = int(message_header.decode('utf-8').strip())
+            message = client_socket.recv(message_length).decode('utf-8')
+            print(f'{username} > {message}')
+    except IOError as e:
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print('Reading error: {}'.format(str(e)))
+            sys.exit()
 
-    print('Received from server: ' + data)  # show in terminal
+        # We just did not receive anything
+        continue
 
-    message = input(" -> ")  # again take input
+    except Exception as e:
+        # Any other exception - something happened, exit
+        print('Reading error: '.format(str(e)))
+        sys.exit()
--- Socket_Prog_Usth/server.py
+++ Socket_Prog_Usth/server.py
@@ -1,24 +1,67 @@
 import socket
+import select
 
-host = socket.gethostname()
-port = 5000  # initiate port no above 1024
+HEADER_LENGTH = 10
 
+IP = "127.0.0.1"
+port = 1024  # initiate port no above 1024
+server_socket = socket.socket()
+#host = socket.gethostname()
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  ##modifies the socket to allow us to reuse the address
 server_socket = socket.socket()  # get instance
  # look closely. The bind() function takes tuple as argument
-server_socket.bind((host, port))  # bind host address and port together
+server_socket.bind((IP, port))  # bind host address and port together
 
     # configure how many client the server can listen simultaneously
-server_socket.listen(2)
-conn, address = server_socket.accept()  # accept new connection
-print("Connection from: " + str(address))
-while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-    data = conn.recv(1024).decode()
-    if not data:
-            # if data is not received break
-        break
-    print("from connected user: " + str(data))
-    data = input(' -> ')
-    conn.send(data.encode())  # send data to the client
+server_socket.listen()
+## creating dict of clients and list of sockets
+sockets_list = [server_socket]
+clients = {}
+#print(f'Listening for connections on {IP}:{port}...')
+#HANDLING messeges received
+def receive_messege(client_socket):
+    try:
+        message_header = client_socket.recv(HEADER_LENGTH)
 
-conn.close()  # close the connection
+        if not len(message_header):
+            return False
+
+        message_length = int(message_header.decode('utf-8').strip())
+        return {'header': message_header, 'data': client_socket.recv(message_length)}
+    except:
+        return False
+while True:
+    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+    for notified_socket in read_sockets:
+        if notified_socket == server_socket:
+            client_socket, client_address = server_socket.accept()
+            user = receive_messege(client_socket)
+            if user is False:
+                continue
+            sockets_list.append(client_socket)
+            clients[client_socket] = user
+            print('Accepted new connection from {}:{}, username: {}'.format(*client_address, user['data'].decode('utf-8')))
+        else:
+            message = receive_messege(notified_socket)
+            if message is False:
+                print('Closed connection from: {}'.format(clients[notified_socket]['data'].decode('utf-8')))
+                sockets_list.remove(notified_socket)
+                del clients[notified_socket]
+
+                continue
+            user = clients[notified_socket]
+
+            print(f'Received message from {user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
+            for client_socket in clients:
+                if client_socket != notified_socket:
+                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+            ##handle for the exception/error sockets with:
+            for notified_socket in exception_sockets:
+                # Remove from list for socket.socket()
+                sockets_list.remove(notified_socket)
+
+                # Remove from our list of users
+                del clients[notified_socket]
+
+
+
